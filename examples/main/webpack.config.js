122d4da434dcf50139d72ec8af029be72e1ffaa6
Rostyslav Khanas <rtkhanas@gmail.com>
2016-06-24 17:58:31 +0900

    Ref

--- examples/main/webpack.config.js
+++ examples/main/webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
 	entry: path.resolve(__dirname, 'index.js'),
 	output: {
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(__dirname, ''),
 		filename: 'bundle.js'
 	},
 	resolve: {
