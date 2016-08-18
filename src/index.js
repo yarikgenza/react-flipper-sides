35f58f513f857acc598734b9096ffaa9346c6d1e
Rostyslav Khanas <rtkhanas@gmail.com>
2016-08-18 23:11:59 +0900

    Removed bl, fixed deps

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import { Flipper } from './components/Flipper';
 import { Front } from './components/Front';
 import { Back } from './components/Back';
-import * as PageSideTypesEnum from './enums/page-side-types-enum';
-import * as FlippingActionTypes from './constants/flipping-action-types';
-import { stackFlippingReducers } from './reducers/base-stack-reducers';
 
 export {
   Flipper,
   Front,
   Back,
-  PageSideTypesEnum,
-  FlippingActionTypes,
-  stackFlippingReducers,
 };
