d906053125dfca22878bba51c16d6843dec51e28
Rostyslav Khanas <rtkhanas@gmail.com>
2017-06-01 14:48:16 +0300

    Temp fix for backface-visability webkit comp

--- src/components/Back/components-styled/BackContainer/index.js
+++ src/components/Back/components-styled/BackContainer/index.js
@@ -4,6 +4,7 @@ export default styled.div`
   height: 100%;
   width: 100%;
   backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
   position: absolute;
   transform: rotateY(180deg);
 `;
