a981179a1e178ce50234c5c6f61a5d5ee2b849f4
Rostyslav Khanas <rtkhanas@gmail.com>
2017-06-08 15:06:30 +0300

    Remove manual set -webkit-backface-visibility

--- src/components/Front/components-styled/FrontContainer/index.js
+++ src/components/Front/components-styled/FrontContainer/index.js
@@ -4,7 +4,6 @@ export default styled.div`
   height: 100%;
   width: 100%;
   backface-visibility: hidden;
-  -webkit-backface-visibility: hidden;
   position: absolute;
   transform: rotateY(0deg);
   overflow: hidden;
