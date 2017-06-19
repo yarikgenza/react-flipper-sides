4d198acd592a050a48e129f0595ccc44c16f9ebc
Rostyslav Khanas <rtkhanas@gmail.com>
2017-06-19 17:18:10 +0300

    Add dev deps to experiment with react primitives

--- storybooks/web/.storybook/webpack.config.js
+++ storybooks/web/.storybook/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.json$/, loader: 'json-loader' }
     ]
   }
 };
